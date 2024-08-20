85e08cf76c60c51a6797a0717a7267ab1f71f19e
aji_hermansya <ajihermansya14@gmail.com>
2024-08-20 20:11:41 +0700

    add-image profile-solved

--- src/auth/controllers/user.controller.ts
+++ src/auth/controllers/user.controller.ts
@@ -1,18 +1,20 @@
 import {
-  Body,
   Controller,
+  Get,
   Post,
   Request,
+  Res,
   UploadedFile,
   UseGuards,
-  UseInterceptors,
+  UseInterceptors
 } from '@nestjs/common';
-import { UserService } from '../services/user.service';
-import { JwtGuard } from '../guards/jwt.guard';
-import { FeedPost } from 'src/feed/models/post.interface';
-import { Observable } from 'rxjs';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { saveImageToStorage } from '../helpers/image-storage';
+import { join } from 'path';
+import { Observable, of, switchMap } from 'rxjs';
+import { UpdateResult } from 'typeorm';
+import { JwtGuard } from '../guards/jwt.guard';
+import { isFileExtensionSafe, removeFile, saveImageToStorage } from '../helpers/image-storage';
+import { UserService } from '../services/user.service';
 
 @Controller('user')
 export class UserController {
@@ -21,10 +23,40 @@ export class UserController {
   @UseGuards(JwtGuard)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', saveImageToStorage))
-  
+
   uploadImage(
-    @UploadedFile() file: Express.Multer.File, 
-    @Request() req) : any {
-    return ;
+    @UploadedFile() file: Express.Multer.File,
+    @Request() req): Observable<UpdateResult | {error : string}> {
+    const fileName = file?.filename;
+
+    if (!fileName) return of({error : 'File must be a png, jpg/jpeg'})
+
+    const imagesFolderPath = join(process.cwd(), 'images');
+    const fullImagePath = join(imagesFolderPath + '/' + file.filename);
+
+    return isFileExtensionSafe(fullImagePath).pipe(
+      switchMap((isFileLegit: boolean) => {
+        if (isFileLegit) {
+          const userId = req.user.id;
+          return this.userService.updateUserImageById(userId, fileName)
+
+        }
+        removeFile(fullImagePath);
+        return of({error: 'File content does not match extension!'});
+      }
+    ))
   }
+
+
+  @UseGuards(JwtGuard)
+  @Get('image')
+  findImage(@Request() req, @Res() res) : Observable<Object> {
+    const userId = req.user.id;
+    return this.userService.findImageNameByUserId(userId).pipe(
+      switchMap((imageName : string) => {
+        return of(res.sendFile(imageName, {root: './images'}))
+      })
+    )
+  }
+
 }
--- src/auth/helpers/image-storage.ts
+++ src/auth/helpers/image-storage.ts
@@ -1,12 +1,10 @@
 import { diskStorage } from 'multer';
+import { from, Observable, of, switchMap } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
-
-
-// const fs = require('fs');
-// const FileType = require('file-type');
-
-
 import path = require('path');
+
+const fs = require('fs');
+const FileType = require('file-type');
 
 type validFileExtension = 'png' | 'jpg' | 'jpeg';
 type validMimeType = 'image/png' | 'image/jpg' | 'image/jpeg';
@@ -32,3 +30,37 @@ export const saveImageToStorage = {
     allowedMimeTypes.includes(file.mimetype) ? cb(null, true) : cb(null, false);
   },
 };
+
+export const isFileExtensionSafe = (
+  fullFilePath: string,
+): Observable<boolean> => {
+  return from(FileType.fromFile(fullFilePath)).pipe(
+    switchMap(
+      (fileExtensionAndMimeType: {
+        ext: validFileExtension;
+        mime: validMimeType;
+      }) => {
+        if (!fileExtensionAndMimeType) return of(false);
+
+        const isFileTypeLegit = validFileExtensions.includes(
+          fileExtensionAndMimeType.ext,
+        );
+        const isMimeTypeLegit = validMimeTypes.includes(
+          fileExtensionAndMimeType.mime,
+        );
+
+        const isFileLegit = isFileTypeLegit && isMimeTypeLegit;
+        return of(isFileLegit);
+      },
+    ),
+  );
+};
+
+export const removeFile = (fullFilePath : string) : void => {
+  try {
+    fs.unlinkSync(fullFilePath);
+    
+  } catch (err) {
+    console.error(err)
+  }
+}
